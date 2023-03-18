8f4141f5827edc20f0110d15a58bff758efc8084
kenma-zks <pallawmgr20@gmail.com>
2023-03-18 20:04:11 +0545

    add status filtering for booking

--- src/pages/admin/Booking.tsx
+++ src/pages/admin/Booking.tsx
@@ -8,51 +8,13 @@ import { setBookings } from '../../store/bookingSlice'
 import { useAppDispatch, useAppSelector } from '../../store/hooks'
 import { IBookingData } from '../../types/types'
 
-const DUMMY_BOOKINGS = [
-  {
-    id: 1,
-    first_name: 'John',
-    last_name: 'Doe',
-    booking_date: '2021-10-10',
-    booking_duration: '10:00 AM - 12:00 PM',
-    table_capacity: 2,
-    phone_number: '1234567890',
-    email: '',
-    note: 'I want to book a table for 2 people',
-    status: 'Pending',
-  },
-  {
-    id: 2,
-    first_name: 'John',
-    last_name: 'Doe',
-    booking_date: '2021-10-10',
-    booking_duration: '10:00 AM - 12:00 PM',
-    table_capacity: 2,
-    phone_number: '1234567890',
-    email: '',
-    note: 'I want to book a table for 2 people',
-    status: 'Accepted',
-  },
-  {
-    id: 3,
-    first_name: 'John',
-    last_name: 'Doe',
-    booking_date: '2021-10-11',
-    booking_duration: '10:00 AM - 12:00 PM',
-    table_capacity: 2,
-    phone_number: '1234567890',
-    email: '',
-    note: 'I want to book a table for 2 people',
-    status: 'Rejected',
-  },
-]
-
 const Booking = () => {
   const bookings = useAppSelector((state) => state.bookings.bookings)
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [selectedBooking, setSelectedBooking] = useState<IBookingData | null>(
     null,
   )
+  const [selectedStatus, setSelectedStatus] = useState<string>('All')
   const dispatch = useAppDispatch()
 
   const groupedBookings = bookings.reduce(
@@ -61,7 +23,14 @@ const Booking = () => {
       if (!acc[bookingDate]) {
         acc[bookingDate] = []
       }
-      acc[bookingDate].push(booking)
+
+      if (
+        selectedStatus === 'All' ||
+        booking.status?.toLowerCase() === selectedStatus.toLowerCase()
+      ) {
+        acc[bookingDate].push(booking)
+      }
+
       return acc
     },
     {},
@@ -96,6 +65,8 @@ const Booking = () => {
     setSelectedBooking(booking)
   }
 
+  const statusFilter = 'All'
+
   return (
     <Box minH="100vh" backgroundColor="#EBEEF2" p="4">
       <AdminNavbar />
@@ -105,109 +76,110 @@ const Booking = () => {
             BOOKINGS
           </Text>
         </Box>
-        <HStack w="100%" pb="2">
-          <Box w="15%">
-            <Text color="gray" fontSize={'sm'} pb="2">
-              Filter by Date
-            </Text>
-            <Select
-              fontSize="sm"
-              backgroundColor={'white'}
-              placeholder="This week"
-            ></Select>
-          </Box>
-          <Box w="15%">
-            <Text color="gray" fontSize={'sm'} pb="2">
-              Filter by Status
-            </Text>
-            <Select
-              fontSize="sm"
-              backgroundColor={'white'}
-              placeholder="All"
-            ></Select>
-          </Box>
-        </HStack>
+        <Box w="15%" pb="2">
+          <Text color="gray" fontSize={'sm'} pb="2">
+            Filter by Status
+          </Text>
+          <Select
+            fontSize="sm"
+            backgroundColor={'white'}
+            borderRadius="md"
+            value={selectedStatus}
+            onChange={(e) => setSelectedStatus(e.target.value)}
+          >
+            <option value="All">All</option>
+            <option value="Pending">Pending</option>
+          </Select>
+        </Box>
 
         <Box w="100%">
-          {Object.entries(groupedBookings).map(([date, bookings]) => (
-            <React.Fragment key={date}>
-              <Box pb="4">
-                <Text fontWeight="medium" fontSize="sm" pb="1" color="gray">
-                  {new Date(date).toLocaleDateString('en-US', {
-                    weekday: 'long',
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric',
-                  })}
-                </Text>
-                {bookings.map((booking) => (
-                  <Box
-                    key={booking.id}
-                    w="60%"
-                    p="4"
-                    backgroundColor="white"
-                    borderRadius="md"
-                    mb="2"
-                    _hover={{ boxShadow: 'md', cursor: 'pointer' }}
-                    onClick={() => openModalHandler(booking)}
-                  >
-                    <HStack
-                      w="100%"
-                      alignItems="center"
-                      justifyContent="space-between"
+          {Object.entries(groupedBookings).map(([date, bookings]) => {
+            const filteredBookings =
+              statusFilter === 'All'
+                ? bookings
+                : bookings.filter((booking) => booking.status === statusFilter)
+            if (!filteredBookings.length) return null
+
+            return (
+              <React.Fragment key={date}>
+                <Box pb="4">
+                  <Text fontWeight="medium" fontSize="sm" pb="1" color="gray">
+                    {new Date(date).toLocaleDateString('en-US', {
+                      weekday: 'long',
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })}
+                  </Text>
+                  {filteredBookings.map((booking) => (
+                    <Box
+                      key={booking.id}
+                      w="60%"
+                      p="4"
+                      backgroundColor="white"
+                      borderRadius="md"
+                      mb="2"
+                      _hover={{ boxShadow: 'md', cursor: 'pointer' }}
+                      onClick={() => openModalHandler(booking)}
                     >
-                      <Text fontWeight="medium" fontSize={'sm'} w="40%">
-                        {booking.first_name} {booking.last_name}
-                      </Text>
-                      <Text color="gray" fontSize={'sm'} w="30%">
-                        {booking.booking_duration}
-                      </Text>
-                      <Box
-                        backgroundColor={
-                          booking.status === 'Accepted'
-                            ? 'green.200'
-                            : booking.status === 'Rejected'
-                            ? 'red.200'
-                            : 'gray.300'
-                        }
-                        color="white"
-                        fontSize={'sm'}
-                        w="20%"
-                        py="2"
-                        borderRadius={'md'}
-                        display="flex"
-                        justifyContent="center"
+                      <HStack
+                        w="100%"
+                        alignItems="center"
+                        justifyContent="space-between"
                       >
-                        <HStack spacing={2}>
-                          <WarningIcon />
-                          <Text
-                            fontWeight="medium"
-                            color={
-                              booking.status === 'Accepted'
-                                ? 'green.600'
-                                : booking.status === 'Rejected'
-                                ? 'red.600'
-                                : 'gray.600'
-                            }
-                            textAlign="center"
-                          >
-                            {booking.status}
-                          </Text>
-                        </HStack>
-                      </Box>
-                    </HStack>
-                  </Box>
-                ))}
-                {selectedBooking && (
-                  <EditBookingModal
-                    isOpen={isModalOpen}
-                    onClose={() => setIsModalOpen(false)}
-                    booking={selectedBooking}
-                  />
-                )}
-              </Box>
-            </React.Fragment>
-          ))}
+                        <Text fontWeight="medium" fontSize={'sm'} w="40%">
+                          {booking.first_name} {booking.last_name}
+                        </Text>
+                        <Text color="gray" fontSize={'sm'} w="30%">
+                          {booking.booking_duration}
+                        </Text>
+                        <Box
+                          backgroundColor={
+                            booking.status === 'Accepted'
+                              ? 'green.200'
+                              : booking.status === 'Rejected'
+                              ? 'red.200'
+                              : 'gray.300'
+                          }
+                          color="white"
+                          fontSize={'sm'}
+                          w="20%"
+                          py="2"
+                          borderRadius={'md'}
+                          display="flex"
+                          justifyContent="center"
+                        >
+                          <HStack spacing={2}>
+                            <WarningIcon />
+                            <Text
+                              fontWeight="medium"
+                              color={
+                                booking.status === 'Accepted'
+                                  ? 'green.600'
+                                  : booking.status === 'Rejected'
+                                  ? 'red.600'
+                                  : 'gray.600'
+                              }
+                              textAlign="center"
+                            >
+                              {booking.status}
+                            </Text>
+                          </HStack>
+                        </Box>
+                      </HStack>
+                    </Box>
+                  ))}
+                  {selectedBooking && (
+                    <EditBookingModal
+                      isOpen={isModalOpen}
+                      onClose={() => setIsModalOpen(false)}
+                      booking={selectedBooking}
+                    />
+                  )}
+                </Box>
+              </React.Fragment>
+            )
+          })}
         </Box>
       </VStack>
     </Box>
