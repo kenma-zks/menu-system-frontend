600e7ec42fd08843048358bb469eeab23ce7310e
kenma-zks <pallawmgr20@gmail.com>
2023-03-19 22:15:09 +0545

    add new path booking history

--- src/routes/requireAuth.tsx
+++ src/routes/requireAuth.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLocation, Navigate, Outlet } from 'react-router-dom'
 import Booking from '../pages/admin/Booking'
+import BookingHistory from '../pages/admin/BookingHistory'
 import AdminHome from '../pages/admin/Home'
 import AdminOrders from '../pages/admin/Orders'
 import Products from '../pages/admin/Products'
@@ -22,6 +23,11 @@ const RequireAuth = ({ route }: { route: string }) => {
       return <AdminOrders />
     case 'bookings':
       return <Booking />
+    case 'order-history':
+      return <AdminOrders />
+    case 'booking-history':
+      return <BookingHistory />
+
     default:
       return null
   }
