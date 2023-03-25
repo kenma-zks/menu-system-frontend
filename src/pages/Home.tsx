b005a55f2dd0f6c4b3b9e6041c27f2300f63932f
kenma-zks <pallawmgr20@gmail.com>
2023-03-25 23:03:02 +0545

    remove old home page and add new home page with new design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,177 +1,62 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import { Search2Icon } from "@chakra-ui/icons";
 import {
   Box,
-  Icon,
+  Divider,
   Input,
   InputGroup,
   InputRightElement,
-  Link,
-  Stack,
   Text,
-} from '@chakra-ui/react'
-import art from '../assets/drawkit.png'
-import { SearchIcon } from '@chakra-ui/icons'
-import ButtonComponent from '../components/FormElements/ButtonComponent'
-import MenuItems from '../components/Card/MenuItems'
-import { fetchCategories, fetchFoodDetails } from '../api/api'
-
-interface ICategory {
-  id: number
-  category_name: string
-}
-
-interface IFoodDetails {
-  id: number
-  food_name: string
-  food_price: number
-  food_image: string
-}
+  VStack,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import CategoryItemList from "../components/Card/CategoryItemList";
+import MenuItemList from "../components/Card/MenuItemList";
+import { ICategoryData } from "../types/types";
 
 const Home = () => {
-  const [activeTag, setActiveTag] = useState('Pizza')
-
-  const [categories, setCategories] = useState<ICategory[]>([])
-
-  const [foodDetails, setFoodDetails] = useState<IFoodDetails[]>([])
-
-  const fetchCategoriesCallback = useCallback(() => {
-    console.log('fetchCategoriesCallback')
-    fetchCategories<ICategory[]>().then((data) => {
-      const transformedData = data.map((item) => {
-        return {
-          id: item.id,
-          category_name: item.category_name,
-        }
-      })
-      setCategories(transformedData)
-    })
-  }, [])
+  const [selectedCategoryItem, setSelectedCategoryItem] =
+    useState<ICategoryData | null>(null);
 
   useEffect(() => {
-    fetchCategoriesCallback()
-  }, [fetchCategoriesCallback])
-
-  const fetchFoodDetailsCallback = useCallback(() => {
-    console.log('fetchFoodDetailsCallback')
-    fetchFoodDetails<IFoodDetails[]>().then((data) => {
-      const transformedData = data.map((item) => {
-        return {
-          id: item.id,
-          food_name: item.food_name,
-          food_price: item.food_price,
-          food_image: item.food_image,
-        }
-      })
-      setFoodDetails(transformedData)
-    })
-  }, [])
-
-  useEffect(() => {
-    fetchFoodDetailsCallback()
-  }, [fetchFoodDetailsCallback])
+    setSelectedCategoryItem({ id: 0, category_name: "All" });
+  }, []);
 
   return (
-    <Box h="100vh">
-      <Box
-        backgroundImage={`url(${art})`}
-        h="40vh"
-        width="100%"
-        backgroundPosition="center"
-        backgroundSize="cover"
-        backgroundRepeat="no-repeat"
-      >
-        <InputGroup
-          background="white"
-          width="80%"
-          marginLeft="10%"
-          position="relative"
-          top="78%"
-          borderRadius="2rem"
-          height="2.8rem"
-        >
+    <Box minH="100vh" w="100%" backgroundColor={"#fcfbfb"}>
+      <Box p="4" w="100%" bgColor={"white"}>
+        <InputGroup backgroundColor={"white"} borderRadius="6px" w="40%">
           <Input
-            placeholder="Search for something tasty..."
-            borderRadius="2rem"
-            height="2.8rem"
-            fontSize={{ base: '0.8rem', md: '1rem' }}
+            type="text"
+            placeholder="Search menu"
+            _placeholder={{ color: "black" }}
           />
-          <InputRightElement pointerEvents="none" height="2.8rem">
-            <Icon
-              as={SearchIcon}
-              color="white"
-              background="#53946B"
-              borderRadius="2rem"
-              height="2rem"
-              width="2rem"
-              padding="0.5rem"
-              marginRight="0.5rem"
-            />
-          </InputRightElement>
+          <InputRightElement pointerEvents="none" children={<Search2Icon />} />
         </InputGroup>
       </Box>
-      <Box width="100%" background="#F2F2F2" height="60vh">
-        <Box
-          height="15vh"
-          padding="15px"
-          fontSize={{ base: '0.9rem', md: '1rem', lg: '1.2rem' }}
-        >
-          <Box display="flex" justifyContent="space-between">
-            <Text fontWeight="semibold">Categories</Text>
-            <Link fontWeight="bold" color="#854224" href="#">
-              See all
-            </Link>
-          </Box>
-          <Stack
-            overflowX={{ base: 'scroll', md: 'hidden' }}
-            direction="row"
-            spacing={2}
-            align="center"
-            height="100%"
-          >
-            {categories.map((category) => {
-              return (
-                <ButtonComponent
-                  key={category.id}
-                  name={category.category_name}
-                  setActiveTag={setActiveTag}
-                  isActive={activeTag === category.category_name}
-                />
-              )
-            })}
-          </Stack>
-        </Box>
-        <Box
-          padding="15px"
-          fontSize={{ base: '0.9rem', md: '1rem', lg: '1.2rem' }}
-        >
-          <Box display="flex" justifyContent="space-between" pb="1rem">
-            <Text fontWeight="semibold">Menu</Text>
-            <Link fontWeight="bold" color="#854224" href="#">
-              See all
-            </Link>
-          </Box>
-          <Stack
-            overflowX={{ base: 'scroll', md: 'hidden' }}
-            direction="row"
-            spacing={2}
-            align="center"
-            height="100%"
-          >
-            {foodDetails.map((food) => {
-              return (
-                <MenuItems
-                  key={food.id}
-                  name={food.food_name}
-                  price={food.food_price}
-                  imageURL={food.food_image}
-                />
-              )
-            })}
-          </Stack>
-        </Box>
+      <Divider borderBottom="1px" />
+      <Box p="4">
+        <VStack alignItems={"flex-start"}>
+          <Text fontSize="lg" fontWeight="semibold" pb="2">
+            Categories
+          </Text>
+
+          <CategoryItemList setSelectedCategoryItem={setSelectedCategoryItem} />
+
+          {selectedCategoryItem === null && (
+            <MenuItemList selectedCategoryId={0} />
+          )}
+          {selectedCategoryItem && (
+            <>
+              <Text fontSize="lg" fontWeight="semibold" pt="2" pb={"2"}>
+                Select Menu
+              </Text>
+              <MenuItemList selectedCategoryId={selectedCategoryItem.id} />
+            </>
+          )}
+        </VStack>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
