a8b729e37715b87a31a3613320f629553113f1eb
kenma-zks <pallawmgr20@gmail.com>
2023-03-28 22:27:07 +0545

    add functionality for menu items detail and cart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import { Search2Icon } from "@chakra-ui/icons";
 import {
   Box,
   Divider,
+  HStack,
+  IconButton,
   Input,
   InputGroup,
   InputRightElement,
@@ -9,29 +11,56 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import { FiShoppingCart } from "react-icons/fi";
 import CategoryItemList from "../components/Card/CategoryItemList";
 import MenuItemList from "../components/Card/MenuItemList";
-import { ICategoryData } from "../types/types";
+import CartDrawer from "../components/UI/CartDrawer";
+import { ICategoryData, IProductData } from "../types/types";
 
 const Home = () => {
   const [selectedCategoryItem, setSelectedCategoryItem] =
     useState<ICategoryData | null>(null);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   useEffect(() => {
     setSelectedCategoryItem({ id: 0, category_name: "All" });
   }, []);
 
+  const openCartDrawer = () => {
+    setIsDrawerOpen(true);
+  };
+
   return (
     <Box minH="100vh" w="100%" backgroundColor={"#fcfbfb"}>
       <Box p="4" w="100%" bgColor={"white"}>
-        <InputGroup backgroundColor={"white"} borderRadius="6px" w="40%">
-          <Input
-            type="text"
-            placeholder="Search menu"
-            _placeholder={{ color: "black" }}
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          w="100%"
+          pr="8"
+        >
+          <InputGroup backgroundColor={"white"} borderRadius="6px" w="40%">
+            <Input
+              type="text"
+              placeholder="Search menu"
+              _placeholder={{ color: "black" }}
+            />
+            <InputRightElement
+              pointerEvents="none"
+              children={<Search2Icon />}
+            />
+          </InputGroup>
+          <IconButton
+            aria-label="Cart"
+            icon={<FiShoppingCart />}
+            borderRadius="full"
+            bg="orange.300"
+            _hover={{ bg: "orange.400" }}
+            onClick={() => {
+              openCartDrawer();
+            }}
           />
-          <InputRightElement pointerEvents="none" children={<Search2Icon />} />
-        </InputGroup>
+        </HStack>
       </Box>
       <Divider borderBottom="1px" />
       <Box p="4">
@@ -53,6 +82,10 @@ const Home = () => {
               <MenuItemList selectedCategoryId={selectedCategoryItem.id} />
             </>
           )}
+          <CartDrawer
+            isOpen={isDrawerOpen}
+            onClose={() => setIsDrawerOpen(false)}
+          />
         </VStack>
       </Box>
     </Box>
