e81b52d8f569090500f26b586a3d28b9a3f257bb
kenma-zks <pallawmgr20@gmail.com>
2023-04-01 21:11:08 +0545

    add interface CartItem[]

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,55 +1,63 @@
 export interface IBookingData {
-  id?: number
-  first_name: string
-  last_name: string
-  email: string
-  phone_number: string
-  table_capacity: number | string
-  booking_date: string
-  booking_duration: string
-  note: string
-  status?: string
+  id?: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string;
+  table_capacity: number | string;
+  booking_date: string;
+  booking_duration: string;
+  note: string;
+  status?: string;
 }
 
 export interface ILoginData {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
 export interface IRegisterData {
-  first_name: string
-  last_name: string
-  email: string
-  phone_number: string
-  password: string
-  confirm_password: string
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string;
+  password: string;
+  confirm_password: string;
 }
 
 export interface IProductData {
-  id?: number
-  category_id: number | string
-  food_name: string
-  food_price: number | string
-  food_description: string
-  food_image?: File | string
-  food_available: boolean
+  id?: number;
+  category_id: number | string;
+  food_name: string;
+  food_price: number | string;
+  food_description: string;
+  food_image?: File | string;
+  food_available: boolean;
 }
 
 export interface ICategoryData {
-  id: number
-  category_name: string
+  id: number;
+  category_name: string;
 }
 
 export interface tokenState {
-  access: string
-  refresh: string
+  access: string;
+  refresh: string;
 }
 
 export interface userState {
-  id: number
+  id: number;
 }
 
 export interface authState {
-  authTokens: null | tokenState
-  user: null | userState
+  authTokens: null | tokenState;
+  user: null | userState;
+}
+
+export interface CartItem {
+  id?: number;
+  food_name?: string;
+  food_price?: string | number;
+  quantity: number;
+  totalAmount?: number | string;
 }
