c2449fc84744efea5637046c805dfc1c1dcdc2ad
kenma-zks <pallawmgr20@gmail.com>
2023-04-08 23:44:29 +0545

    add endpoint for forgot password

--- src/store/authApiSlice.tsx
+++ src/store/authApiSlice.tsx
@@ -1,40 +1,49 @@
-import { ILoginData, IRegisterData } from '../types/types'
-import { apiSlice } from './api/apiSlice'
+import { ILoginData, IRegisterData, IResetPasswordData } from "../types/types";
+import { apiSlice } from "./api/apiSlice";
 
 export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (build) => ({
     verifyLogin: build.mutation({
       query: (loginInfo: ILoginData) => ({
-        url: 'token/',
-        method: 'POST',
+        url: "token/",
+        method: "POST",
         body: {
           email: loginInfo.email,
           password: loginInfo.password,
         },
-        include: 'credentials',
+        include: "credentials",
       }),
     }),
 
     registerUser: build.mutation({
       query: (registerInfo: IRegisterData) => ({
-        url: 'register/',
-        method: 'POST',
+        url: "register/",
+        method: "POST",
         body: registerInfo,
-        include: 'credentials',
+        include: "credentials",
+      }),
+    }),
+
+    verifyEmail: build.mutation({
+      query: (emailInfo: IResetPasswordData) => ({
+        url: "forgot-password/",
+        method: "POST",
+        body: { email: emailInfo.email },
       }),
     }),
 
     logoutUser: build.mutation<void, void>({
       query: () => ({
-        url: 'logout/',
-        method: 'GET',
+        url: "logout/",
+        method: "GET",
       }),
     }),
   }),
-})
+});
 
 export const {
   useVerifyLoginMutation,
   useRegisterUserMutation,
+  useVerifyEmailMutation,
   useLogoutUserMutation,
-} = authApiSlice
+} = authApiSlice;
