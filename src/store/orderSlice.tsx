78f060e881fdb086925608de3af18198db6351d2
kenma-zks <pallawmgr20@gmail.com>
2023-04-12 21:04:38 +0545

    add new addorder reducer

---
+++ src/store/orderSlice.tsx
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IOrderData } from "../types/types";
+
+interface OrderState {
+  orders: IOrderData[];
+}
+
+const initialState: OrderState = {
+  orders: [],
+};
+
+const orderSlice = createSlice({
+  name: "orders",
+  initialState: initialState,
+  reducers: {
+    setOrders(state, action: PayloadAction<IOrderData[]>) {
+      state.orders = action.payload;
+    },
+    deleteOrder(state, action: PayloadAction<number | undefined>) {
+      state.orders = state.orders.filter(
+        (order) => order.order_id !== action.payload
+      );
+    },
+    addOrder(state, action: PayloadAction<IOrderData>) {
+      state.orders.push(action.payload);
+    },
+  },
+});
+
+export const { setOrders, deleteOrder } = orderSlice.actions;
+
+export default orderSlice.reducer;
